a31a71ca80948583734893136931f10935bdfa83
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-09-02 14:33:25 +0100

    finished square root function

--- Taschenrechner/Wurzeln.c
+++ Taschenrechner/Wurzeln.c
@@ -4,3 +4,30 @@
  * and open the template in the editor.
  */
 
+// ! By Timofey Makhankov
+#include <stdio.h>
+
+int calculateSQRT(int num){
+    short calculating = 1;
+    short is_running = 1;
+    while (is_running){
+    int input = 0;
+    printf("\nGive a Digit:");
+    scanf("%d", &result)
+    if(input < 1){
+        printf("Please try again!\n")
+    }else{
+        while(calculating){
+            int i = 1;
+            int compare = i * i;
+            if(compare == input){
+                return i;
+                calculating = 0;
+            } else if (compare > input){
+                printf("Couldn't get the square root of %d", input);
+                calculation = 0;
+            }
+        }
+    }is_running = 0;
+    }
+}
--- Taschenrechner/Wurzeln.h
+++ Taschenrechner/Wurzeln.h
@@ -1,7 +1,7 @@
 #ifndef WURZELN_H
 #define WURZELN_H
 
-
+calculateSQRT(int num);
 
 #endif /* WURZELN_H */
 
