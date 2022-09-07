fbd53a3b94b3cf43ecaf92b14ac6313c6fc8a0ec
Valentin Nussbaumer <112619659+nussbaumerv@users.noreply.github.com>
2022-09-07 11:34:48 +0200

    add dreieck berechung files

--- Taschenrechner/Dreiecksberechnung.c
+++ Taschenrechner/Dreiecksberechnung.c
@@ -1,6 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float dreiecksberechnung(){
+    float firstNumber;
+    float secondNumber;
+
+    printf("Type the length of A in cm: ");
+    scanf("%f", &firstNumber);
+
+    printf("Type the length of B in cm: ");
+    scanf("%f", &secondNumber);
+
+    float result = (firstNumber * firstNumber) + (secondNumber * secondNumber);
+    return result;
+
+}
+
 
