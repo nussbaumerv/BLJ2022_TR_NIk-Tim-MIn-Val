1c530053ea2b82cf4ded24d0812ebea2bc73b418
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-09-07 11:49:36 -0600

    added functions.h

---
+++ Taschenrechner/functions.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTIONS.H
+#define FUNCTIONS.H
+
+int addition();
+int subtraktion();
+int multiplikation ();
+float division();
+int exponenten();
+int wurzeln();
+int fakultat();
+
+
+#endif // !FUNCTIONS.H
