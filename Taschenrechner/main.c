2b3aa69e3ff96d0f09fdb306185c6b6255c42306
Valentin Nussbaumer <valentin.nussbaumer@noseryoung.com>
2022-09-07 10:25:06 +0200

    main function while loop

--- Taschenrechner/main.c
+++ Taschenrechner/main.c
@@ -10,38 +10,60 @@
 
 int main(int argc, char **argv)
 {
-    char chooseFunction;
-    printf("Welcome to our calculator\n");
-    scanf("%c", &chooseFunction);
+     char firstInput;
+     int isRunning = 1;
+     printf("Welcome to ou\n");
 
-    if (chooseFunction =='a' || chooseFunction == 'A')
-    {
-        printf("%d\n", addition());
-    }
-    else if (chooseFunction == 's' || chooseFunction =='S')
-    {
-          printf("%d\n", subtraktion());
-    }
-    else if (chooseFunction == 'm' || chooseFunction == 'M')
-    {
-         printf("%d\n", multiplikation());
-    }
-    else if (chooseFunction == 'd' || chooseFunction == 'D')
-    {
-         printf("%d\n", division());
-    }
-    else if (chooseFunction == 'f' || chooseFunction == 'F')
-    {
-         printf("%d\n", fakultat());
-    }
-    else if (chooseFunction == 'w' || chooseFunction == 'W')
-    {
-         printf("%d\n", wurzeln());
-    }
-    else if (chooseFunction == 'e' || chooseFunction == 'E')
-    {
-       printf("%d\n", exponenten());
-    }
+     while (isRunning)
+     {
+          fflush(stdin);
+          printf("\nEnter Y to start or X to exit: ");
+          scanf("%c", &firstInput);
+          fflush(stdin);
 
-    return (EXIT_SUCCESS);
+          if (firstInput == 'y' || firstInput == 'Y')
+          {
+               char chooseFunction;
+               printf("What tool do you wane use?\n");
+               scanf("%c", &chooseFunction);
+               fflush(stdin);
+
+               if (chooseFunction == 'a' || chooseFunction == 'A')
+               {
+                    printf("%d\n", addition());
+               }
+               else if (chooseFunction == 's' || chooseFunction == 'S')
+               {
+                    printf("%d\n", subtraktion());
+               }
+               else if (chooseFunction == 'm' || chooseFunction == 'M')
+               {
+                    printf("%d\n", multiplikation());
+               }
+               else if (chooseFunction == 'd' || chooseFunction == 'D')
+               {
+                    printf("%d\n", division());
+               }
+               else if (chooseFunction == 'f' || chooseFunction == 'F')
+               {
+                    printf("%d\n", fakultat());
+               }
+               else if (chooseFunction == 'w' || chooseFunction == 'W')
+               {
+                    printf("%d\n", wurzeln());
+               }
+               else if (chooseFunction == 'e' || chooseFunction == 'E')
+               {
+                    printf("%d\n", exponenten());
+               }
+          }
+          if (firstInput == 'x' || firstInput == 'X')
+
+          {
+               printf("Thanks for using our programm");
+               isRunning = 0;
+          }
+     }
+     system("pause");
+     return (EXIT_SUCCESS);
 }
