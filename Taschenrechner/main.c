b7b7114fa359a4c4e2b51c638d1ba1a19423882a
Timofey-Makhankov <timofey.makahnkov@noseryoung.com>
2022-09-07 12:19:42 -0600

    made all functions to one file

--- Taschenrechner/main.c
+++ Taschenrechner/main.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "Addition.h"
-//#include "Division.h"
-//#include "Exponenten.h"
-//#include "Subtraktion.h"
-//#include "Fakultat.h"
-//#include "Wurzeln.h"
-//#include "Multiplikation.h"
-
 #include "functions.h"
 
 int main(int argc, char **argv)
